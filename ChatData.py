830898c73ca9ac36e1b93fb40a040e3809958a32
jamesnogra <jamesnogra@gmail.com>
2023-12-30 17:37:26 +0800

    First push.

---
+++ ChatData.py
@@ -0,0 +1,40 @@
+from torch.utils.data import Dataset
+import os, csv
+
+START_STRING_TOKEN = '<startofstring>'
+END_STRING_TOKEN = '<endofstring>'
+BOT_TOKEN = '<bot>:'
+MAX_LENGTH = 128
+
+class ChatData(Dataset):
+
+	def __init__(self, training_data_dir, tokenizer):
+		self.X = []
+		for filename in os.listdir(training_data_dir):
+			file_path = os.path.join(training_data_dir, filename)
+			print(f'Loading file {file_path}')
+			with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+				# Use the csv.reader to handle quoted strings
+				csv_reader = csv.reader(file)
+				next(csv_reader)  # Skip header row
+				for row in csv_reader:
+					# Ensure there are two elements in the row
+					if len(row) == 2:
+						question, answer = row
+						self.X.append(f'{START_STRING_TOKEN}{question.lower()} {BOT_TOKEN} {answer.lower()}{END_STRING_TOKEN}')
+		self.X_encoded = tokenizer(
+			self.X,
+			max_length=MAX_LENGTH,
+			padding='max_length',
+			truncation=True,
+			return_tensors='pt'
+		)
+		self.input_ids = self.X_encoded['input_ids']
+		self.attention_mask = self.X_encoded['attention_mask']
+		print(f'First sample data: {self.X[0]}')
+
+	def __len__(self):
+		return len(self.X)
+
+	def __getitem__(self, idx):
+		return (self.input_ids[idx], self.attention_mask[idx])
---
+++ train.py
@@ -0,0 +1,69 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
+from ChatData import ChatData, START_STRING_TOKEN, END_STRING_TOKEN, BOT_TOKEN
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torch
+
+DATA_DIR = 'texts_qa'
+EPOCHS = 100
+SAVED_MODEL_NAME = 'gpt2-custom'
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# Add the special tokens
+tokenizer.add_special_tokens({
+	'pad_token': '<pad>',
+	'bos_token': START_STRING_TOKEN,
+	'eos_token': END_STRING_TOKEN
+})
+tokenizer.add_tokens([BOT_TOKEN])
+
+# Define your own GPT2LMHeadModel configuration
+model_config = GPT2Config(
+	vocab_size=tokenizer.vocab_size,
+	n_positions=256,  # Adjust the maximum position length if necessary (gpt2 has 1024)
+	n_embd=256,  # Adjust the embedding dimension (gpt2 has 768)
+	n_layer=8,  # Adjust the number of layers (gpt2 has 12)
+	n_head=8,  # Adjust the number of attention heads (gpt2 has 12)
+	intermediate_size=2048,  # Adjust the intermediate size (gpt2 has 3072)
+)
+# Create model
+model = GPT2LMHeadModel(config=model_config)
+# reset model tokenizers since we added some custom tokens
+model.resize_token_embeddings(len(tokenizer))
+model = model.to(DEVICE)
+
+# Tokenize data from the directory
+chat_data = ChatData(DATA_DIR, tokenizer)
+chat_data = DataLoader(chat_data, batch_size=8)
+
+model.train()
+optim = Adam(model.parameters(), lr=1e-4)
+
+def infer(text_input):
+	text_input = f'{START_STRING_TOKEN}{text_input.lower()} {BOT_TOKEN}'
+	text_input = tokenizer(text_input, return_tensors='pt')
+	X = text_input['input_ids'].to(DEVICE)
+	a = text_input['attention_mask'].to(DEVICE)
+	output = model.generate(X, attention_mask=a)
+	return tokenizer.decode(output[0])
+
+def train(chat_data, model, optim):
+	for i in range(EPOCHS):
+		for X, a in chat_data:
+			X = X.to(DEVICE)
+			a = a.to(DEVICE)
+			optim.zero_grad()
+			loss = model(X, attention_mask=a, labels=X).loss
+			loss.backward()
+			optim.step()
+		print(f'Epoch: {i+1}\tLoss: {loss.item()}')
+		#print(Sentence: {infer('mahal kita')})
+	#torch.save(model.state_dict(), f'{SAVED_MODEL_NAME}.pt')
+	# Save the fine-tuned model
+	model.save_pretrained(SAVED_MODEL_NAME)
+	tokenizer.save_pretrained(SAVED_MODEL_NAME)
+
+train(chat_data, model, optim)
+print(infer('mahal kita'))
